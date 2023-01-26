03db9a9438ba2dd68a2b55b0506f26c7b42e07e2
정선우 <laborlawseon@gmail.com>
2023-01-27 06:04:00 +0900

    한 번만 등장한 문자 완료

---
+++ src/Level_0/Main17.java
@@ -0,0 +1,15 @@
+package Level_0;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main17 {
+	public static void main(String[] args) {
+		solution("happy birthday!");
+	}
+
+	static int solution(String message) {
+		return message.length() * 2;
+	}
+}
--- src/TestClass.java
+++ src/TestClass.java
@@ -1,21 +1,9 @@
-import java.util.Arrays;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class TestClass {
 
     public static void main(String[] args) {
-        // 약수 구하기
-        solution(new int[]{4, 5, 6, 7, 8, 9, 10, 11, 12});
 
-    }
-
-    static int solution(int[] a) {
-
-        IntStream intStream = Arrays.stream(a);
-        intStream.limit(5).sorted().forEach(System.out::println);
-
-
-        return 0;
+        String A = "선우";
+        String B = "선우";
     }
 }
