c2cb0cdbfe396d940ed2995c96ce19fbcb4324e0
정선우 <laborlawseon@gmail.com>
2023-01-29 14:12:42 +0900

    암호 해독 진행 중

---
+++ src/Level_0/Main20.java
@@ -0,0 +1,18 @@
+package Level_0;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main20 {
+	public static void main(String[] args) {
+		solution("dfjardstddetckdaccccdegk", 4);
+	}
+
+	static public String solution(String cipher, int code) {
+
+		System.out.println(cipher.charAt(code));
+
+		return null;
+	}
+}
