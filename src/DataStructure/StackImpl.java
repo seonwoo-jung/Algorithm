1c9fd9af6c899847421b9124522571d7782cfb88
정선우 <laborlawseon@gmail.com>
2023-02-11 22:21:01 +0900

    자바로 stack 구현하기

---
+++ src/DataStructure/StackImpl.java
@@ -0,0 +1,24 @@
+package DataStructure;
+
+import java.util.Stack;
+
+public class StackImpl {
+	public static void main(String[] args) {
+
+		Stack<Integer> stack = new Stack<>();
+
+		// 0, 1, 2, 3, 4
+		for (int i = 0; i < 5; i++) {
+			stack.push(i);
+		}
+
+		System.out.println("stack = " + stack);
+
+		stack.pop();
+		stack.pop();
+
+		stack.push(5);
+
+		System.out.println("stack = " + stack);
+	}
+}
