cb1d186af78b217b8373675a351ec09c7c608f87
seonwoo_jung <laborlawseon@gmail.com>
2023-06-19 16:31:18 +0900

    study

---
+++ src/programmers/level_0/Main31.java
@@ -0,0 +1,29 @@
+package programmers.level_0;
+
+import static java.lang.String.format;
+
+public class Main31 {
+    public static void main(String[] args) {
+        System.out.println(solution(1081));
+    }
+
+    static int solution(int chicken) {
+        int answer = chicken / 9;
+        if (chicken > 1 && chicken % 9 == 0) {
+            answer--;
+        }
+        return answer;
+    }
+
+    static int solution2(int chicken) {
+        int coupon = chicken;
+        int count = 0;
+
+        while (coupon >= 10) {
+            int leftCoupon = coupon % 10;
+            count += coupon / 10;
+            coupon = leftCoupon + coupon / 10;
+        }
+        return count;
+    }
+}
