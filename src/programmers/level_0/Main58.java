dd5b11d9bf3be5eb8902426a26a38d0732302d26
seonwoo_jung <laborlawseon@gmail.com>
2023-07-29 15:56:31 +0900

    프로그래머스 코딩테스트

---
+++ src/programmers/level_0/Main58.java
@@ -0,0 +1,15 @@
+package programmers.level_0;
+
+public class Main58 {
+
+    public static void main(String[] args) {
+        int[] param = {4, 455, 6, 4, -1, 45, 6};
+        String direction = "right";
+        solution(param, direction);
+    }
+
+    static int[] solution(int[] numbers, String direction) {
+        int[] answer = {};
+        return answer;
+    }
+}
