470cb93feaa29b1242e64abfe4f5fe73e184c347
정선우 <laborlawseon@gmail.com>
2023-07-29 23:54:10 +0900

    study

---
+++ src/algorithm/ch05/RecurX1.java
@@ -0,0 +1,39 @@
+package algorithm.ch05;
+
+import algorithm.ch04.IntStack;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class RecurX1 {
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(in);
+
+        System.out.print("정수를 입력하세요.: ");
+        int x = stdIn.nextInt();
+
+        recur(x);
+    }
+
+    static void recur(int n) {
+        IntStack s = new IntStack(n);
+
+        while (true) {
+            if (n > 0) {
+                s.push(n);
+                n = n - 1;
+                continue;
+            }
+
+            if (s.isEmpty() != true) {
+                n = s.pop();
+                System.out.println(n);
+                n = n - 2;
+                continue;
+            }
+            break;
+        }
+    }
+}
