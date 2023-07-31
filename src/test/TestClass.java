bdb5cd4f39ef87ae952073fffa6f202faf315705
정선우 <laborlawseon@gmail.com>
2023-07-31 18:44:00 +0900

    study

---
+++ src/test/TestClass.java
@@ -0,0 +1,11 @@
+package test;
+
+public class TestClass {
+    public static void main(String[] args) {
+//        String name = "정선";
+//        boolean result = name.matches(".*우$");
+//        System.out.println("result = " + result);
+        int num = 2;
+        System.out.println(num << 9);
+    }
+}
