a68721eb89957d376dd76ae7e86ee51ed6a7a765
seonwoo_jung <laborlawseon@gmail.com>
2023-10-18 18:15:17 +0900

    코딩테스트 학습중

--- src/inflearn/ch03/Main4.java
+++ src/inflearn/ch03/Main4.java
@@ -1,16 +1,14 @@
 package inflearn.ch03;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 import static java.lang.Math.max;
 import static java.lang.System.in;
-import static java.util.Arrays.sort;
 
 /**
- * 최대매출
+ * 연속 부분수열
  */
-public class Main3 {
+public class Main4 {
     public static void main(String[] args) {
         Scanner kb = new Scanner(in);
 
@@ -29,21 +27,7 @@ public class Main3 {
 
     // n: 배열 사이즈, k: 연속된 일수
     static int solution(int n, int k, int[] arr) {
-        int answer, sum = 0;
 
-        // 0부터 k번 째까지 합한다.
-        for (int i = 0; i < k; i++) {
-            sum += arr[i];
-        }
-
-        answer = sum;
-
-        // arr[0] + arr[1] + arr[2]와 arr[1] + arr[2] + arr[3] 비교 후 큰 값을 answer에 입력
-        for (int i = k; i < n; i++) {
-            sum += (arr[i] - arr[i - k]);
-            answer = max(answer, sum);
-        }
-
-        return answer;
+        return 0;
     }
 }
