2b5d3819342431bd6250291ee88e04df6ac05780
seonwoo_jung <laborlawseon@gmail.com>
2023-10-22 18:12:09 +0900

    코딩테스트 학습중

---
+++ src/programmers/level_0/Main87.java
@@ -0,0 +1,13 @@
+package programmers.level_0;
+
+public class Main87 {
+    public static void main(String[] args) {
+        solution(1, 2, 3, 4);
+    }
+
+    static int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int[] answer = {};
+
+        return answer;
+    }
+}
