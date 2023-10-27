0f404631ed40d4dbb24c89bc1a26692a0896f401
seonwoo_jung <laborlawseon@gmail.com>
2023-10-27 16:33:44 +0900

    코딩테스트 학습중

---
+++ src/programmers/level_1/Main3.java
@@ -0,0 +1,15 @@
+package programmers.level_1;
+
+public class Main3 {
+    public static void main(String[] args) {
+        solution(
+                new String[]{"SOO","OOO","OOO"},
+                new String[]{"E 2","S 2","W 1"}
+        );
+    }
+
+    static int[] solution(String[] park, String[] routes) {
+        int[] answer = {};
+        return answer;
+    }
+}
