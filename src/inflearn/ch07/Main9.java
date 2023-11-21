01a4590c186df8be9fd46899de7fc4bfcd06738c
seonwoo_jung <laborlawseon@gmail.com>
2023-11-21 16:24:03 +0900

    코딩테스트 학습중

--- src/inflearn/ch07/Main9.java
+++ src/inflearn/ch07/Main9.java
@@ -1,5 +1,7 @@
 package inflearn.ch07;
 
+import static java.lang.Math.min;
+
 /**
  *  Tree 말단노드까지의 가장 짧은 경로(DFS)
  */
@@ -22,7 +24,7 @@ public class Main9 {
         if (root.lt == null && root.rt == null) {
             return L;
         } else {
-            return Math.min(DFS(L + 1, root.lt), DFS(L + 1, root.rt));
+            return min(DFS(L + 1, root.lt), DFS(L + 1, root.rt));
         }
     }
 
