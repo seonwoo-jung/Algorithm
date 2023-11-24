fad4b779352c8fbc6c04b1bc25c689772701ec68
seonwoo_jung <laborlawseon@gmail.com>
2023-11-24 10:57:16 +0900

    코딩테스트 학습중

---
+++ src/inflearn/ch08/Main3.java
@@ -0,0 +1,42 @@
+package inflearn.ch08;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+/**
+ * 최대점수 구하기
+ */
+public class Main3 {
+
+    static String answer = "NO";
+    static int n, total = 0;
+    boolean flag = false;
+
+    void DFS(int L, int sum, int[] arr) {
+        if (flag) return;
+        if (sum > total / 2) return;
+        if (L == n) {
+            if ((total - sum) == sum) {
+                answer = "YES";
+                flag = true;
+            }
+        } else {
+            DFS(L + 1, sum + arr[L], arr);
+            DFS(L + 1, sum, arr);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(in);
+        n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+            total += arr[i];
+        }
+        Main3 main = new Main3();
+        main.DFS(0, 0, arr);
+        System.out.println(answer);
+    }
+}
