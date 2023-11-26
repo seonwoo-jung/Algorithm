e4d931a5c897e73c17b9c9573809e049ad426390
seonwoo_jung <laborlawseon@gmail.com>
2023-11-26 18:27:13 +0900

    코딩테스트 학습중

---
+++ src/inflearn/ch08/Main9.java
@@ -0,0 +1,18 @@
+package inflearn.ch08;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+/**
+ *
+ */
+public class Main9 {
+    static void DFS(int L, int sum) {
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(in);
+        DFS(0, 0);
+    }
+}
