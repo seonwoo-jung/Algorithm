d081af25ef26fca1f6f27b6ecb5d5e601bad45f5
seonwoo_jung <laborlawseon@gmail.com>
2023-11-27 14:53:05 +0900

    코딩테스트 학습중

--- src/inflearn/ch08/Main9.java
+++ src/inflearn/ch08/Main9.java
@@ -5,14 +5,17 @@ import java.util.Scanner;
 import static java.lang.System.in;
 
 /**
- *
+ * 조합구하기
  */
 public class Main9 {
     static void DFS(int L, int sum) {
+
     }
 
     public static void main(String[] args) {
         Scanner kb = new Scanner(in);
+        int n = kb.nextInt();
+        int m = kb.nextInt();
         DFS(0, 0);
     }
 }
