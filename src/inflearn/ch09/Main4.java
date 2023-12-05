8d64c91d22698eb19e598db114f17e7719ccd4e1
seonwoo_jung <laborlawseon@gmail.com>
2023-12-05 14:37:36 +0900

    코딩테스트 학습중

---
+++ src/inflearn/ch09/Main4.java
@@ -0,0 +1,15 @@
+package inflearn.ch09;
+
+import java.util.Scanner;
+import static java.lang.System.in;
+
+/**
+ * 최대 수입 스케쥴(우선순위 큐 응용문제)
+ */
+public class Main4 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(in);
+        int n = kb.nextInt();
+
+    }
+}
