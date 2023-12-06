fdbd5606d7bd0417c4bb30bdf5719d60aaacbf55
seonwoo_jung <laborlawseon@gmail.com>
2023-12-06 15:39:54 +0900

    코딩테스트 학습중

---
+++ src/inflearn/ch09/Main5.java
@@ -0,0 +1,18 @@
+package inflearn.ch09;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+/**
+ * 다익스트라 알고리즘
+ */
+public class Main5 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(in);
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+
+
+    }
+}
