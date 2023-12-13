280822de6e43576fc8353ab9c5d7999212efef74
seonwoo_jung <laborlawseon@gmail.com>
2023-12-13 13:18:14 +0900

    자바 알고리즘 문제풀이 - 코테대비 완강

--- src/inflearn/ch10/Main3.java
+++ src/inflearn/ch10/Main3.java
@@ -2,10 +2,11 @@ package inflearn.ch10;
 
 import java.util.Scanner;
 
+import static java.lang.Math.max;
 import static java.lang.System.in;
 
 /**
- * 최대 부분 증가수열
+ * 최대 부분 증가수열(LIS)
  */
 public class Main3 {
 
@@ -16,15 +17,28 @@ public class Main3 {
 
         int n = kb.nextInt();
         int[] arr = new int[n];
-        dy = new int[n];
 
         for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
 
-        System.out.println(solution(n));
+        System.out.println(solution(arr));
     }
 
-    static int solution(int n) {
+    static int solution(int[] arr) {
+        int answer = 0;
+        dy = new int[arr.length];
+        dy[0] = 1;
 
-        return 0;
+        for (int i = 1; i < arr.length; i++) {
+            int max = 0;
+
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[i] > arr[j] && dy[j] > max) {
+                    max = dy[j];
+                }
+                dy[i] = max + 1;
+                answer = max(answer, dy[i]);
+            }
+        }
+        return answer;
     }
 }
