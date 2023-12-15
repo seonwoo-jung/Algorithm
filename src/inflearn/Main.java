795e395b4f997c6c665e63275971a924dbeef51c
seonwoo_jung <laborlawseon@gmail.com>
2023-12-15 16:50:39 +0900

    ch3-2 공통원소 구하기 학습완료

--- src/inflearn/Main.java
+++ src/inflearn/Main.java
@@ -4,33 +4,44 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import static java.lang.System.in;
+import static java.util.Arrays.sort;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner kb = new Scanner(in);
         int n = kb.nextInt();
-        int[] arr1 = new int[n];
-        for (int i = 0; i < n; i++) arr1[i] = kb.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = kb.nextInt();
+
         int m = kb.nextInt();
-        int[] arr2 = new int[m];
-        for (int i = 0; i < m; i++) arr2[i] = kb.nextInt();
-        ArrayList<Integer> solution = solution(n, m, arr1, arr2);
-        System.out.println("solution = " + solution);
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) b[i] = kb.nextInt();
+
+        ArrayList<Integer> solution = solution(n, m, a, b);
+        for (Integer x : solution) {
+            System.out.printf("%d ", x);
+        }
     }
 
-    static ArrayList<Integer> solution(int n, int m, int[] arr1, int[] arr2) {
-        ArrayList<Integer> list = new ArrayList<>();
+    static ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
+        ArrayList<Integer> answer = new ArrayList<>();
         int p1 = 0, p2 = 0;
 
+        sort(a);
+        sort(b);
+
         while (p1 < n && p2 < m) {
-            if (arr1[p1] < arr2[p2]) list.add(arr1[p1++]);
-            else list.add(arr2[p2++]);
+            if (a[p1] == b[p2]) {
+                answer.add(a[p1++]);
+                p2++;
+            } else if (a[p1] < b[p2]) {
+                p1++;
+            } else {
+                p2++;
+            }
         }
 
-        while (p1 < n) list.add(arr1[p1++]);
-        while (p2 < m) list.add(arr2[p2++]);
-
-        return list;
+        return answer;
     }
 }
