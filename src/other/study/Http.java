bdb5cc381b939acd9125fb2e221abb302c1b5fba
정선우 <laborlawseon@gmail.com>
2023-12-27 00:21:11 +0900

    lambda, File I/O, Http 학습

--- src/other/study/Http.java
+++ src/other/study/Http.java
@@ -1,4 +1,4 @@
-package other;
+package other.study;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
---
+++ src/other/study/lambda/UsingLocalVariableExample.java
@@ -0,0 +1,8 @@
+package other.study.lambda;
+
+public class UsingLocalVariableExample {
+    public static void main(String[] args) {
+        UsingLocalVariable ulv = new UsingLocalVariable();
+        ulv.method(20);
+    }
+}
