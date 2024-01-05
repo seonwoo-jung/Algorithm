8e5546baa669a920fd3dbc7d8ea17f7e90883915
seonwoojung <laborlawseon@gmail.com>
2024-01-05 17:19:29 +0900

    프로그래머스 코딩테스트 학습

---
+++ src/programmers/level_1/Main36.java
@@ -0,0 +1,19 @@
+package programmers.level_1;
+
+public class Main36 {
+
+    public static void main(String[] args) {
+        int solution = solution(45);
+        System.out.println("solution = " + solution);
+    }
+
+    static int solution(int n) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(Integer.toString(n, 3));
+        int answer = Integer.parseInt(sb.reverse().toString(), 3);
+
+        return answer;
+    }
+}
+
+
