205417d3c61be9c814b83abff252bbabe0b6f013
seonwoojung <laborlawseon@gmail.com>
2024-01-09 16:51:29 +0900

    프로그래머스 코딩테스트 학습

---
+++ src/programmers/level_1/Main43.java
@@ -0,0 +1,20 @@
+package programmers.level_1;
+
+public class Main43 {
+    public static void main(String[] args) {
+        int solution = solution("10203", "15");
+        System.out.println("solution = " + solution);
+    }
+
+    static int solution(String t, String p) {
+        int len = p.length();
+        long num = Long.parseLong(p);
+        int result = 0;
+
+        for (int i = 0; i < t.length() - len + 1; i++) {
+            long diff = Long.parseLong(t.substring(i, i + len));
+            if (diff <= num) result++;
+        }
+        return result;
+    }
+}
---
+++ src/programmers/level_1/Main44.java
@@ -0,0 +1,47 @@
+package programmers.level_1;
+
+public class Main44 {
+
+    static int[] ch;
+    static int answer = 0;
+
+    public static void main(String[] args) {
+        int[] number = {-2, 3, 0, 2, -5};
+        ch = new int[number.length];
+        DFS(0, 0, number);
+        System.out.println("answer = " + answer);
+    }
+
+    static int solution(int[] number) {
+        int answer = 0;
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i + 1; j < number.length; j++) {
+                for (int k = j + 1; k < number.length; k++) {
+                    int sum = number[i] + number[j] + number[k];
+                    if (sum == 0) answer++;
+                }
+            }
+        }
+        return answer;
+    }
+
+    static void DFS(int L, int s, int[] number) {
+        if (L == 3) {
+            int sum = 0;
+            for (int i = 0; i < number.length; i++) {
+                if (ch[i] == 1) {
+                    sum += number[i];
+                }
+            }
+            if (sum == 0) answer++;
+        } else {
+            for (int i = s; i < number.length; i++) {
+                if (ch[i] == 0) {
+                    ch[i] = 1;
+                    DFS(L + 1, s + i, number);
+                    ch[i] = 0;
+                }
+            }
+        }
+    }
+}
