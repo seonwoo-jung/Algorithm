5ad65824efc3cfb59cef6d1f005e5f2684764077
seonwoo_jung <laborlawseon@gmail.com>
2024-07-29 11:35:17 +0900

    코딩테스트 중급 학습중

--- src/main/java/com/study/algorithm/inflearn/middle/Main1.java
+++ src/main/java/com/study/algorithm/inflearn/middle/Main1.java
@@ -10,39 +10,20 @@ public class Main1 {
 		System.out.println(solution(n, ladder));
 	}
 
-	static int[] solution(int n, int[][] ladder) {
-		int[] answer = {};
-		int[][] graph = new int[n + 1][n + 1];
+	static char[] solution(int n, int[][] ladder) {
+		char[] answer = new char[n];
 
-		for (int i = 0; i < ladder.length; i++) {
-			for (int j = 0; j < ladder[i].length; j++) {
-				if (ladder[i][j] != 0) {
-					int point = ladder[i][j];
-					graph[point][i + 1] = point + 1;
-				}
-			}
+		for (int i = 0; i < n; i++) {
+			answer[i] = (char)(i + 65);
 		}
 
-		for (int i = 1; i < graph.length; i++) {
-			for (int j = 1; j < graph[i].length; j++) {
-				if (graph[i][j] != 0) {
-					i = graph[i][j];
-					j = j + 1;
-				}
-				System.out.printf("graph[%d][%d] = %d\n", i, j, graph[i][j]);
+		for (int[] line : ladder) {
+			for (int x : line) {
+				char tmp = answer[x];
+				answer[x] = answer[x - 1];
+				answer[x - 1] = tmp;
 			}
 		}
-
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= n; j++) {
-				if (graph[i][j] != 0) {
-					i = graph[i][j];
-					j = j + 1;
-				}
-			}
-			System.out.println("i = " + i);
-		}
-
 		return answer;
 	}
 }
