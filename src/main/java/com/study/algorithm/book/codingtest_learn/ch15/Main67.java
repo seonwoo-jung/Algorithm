20f45cbfae3acca65f6ccfe9ad07bced66fc6869
seonwoo_jung <laborlawseon@gmail.com>
2024-08-21 19:30:27 +0900

    study algorithm

---
+++ src/main/java/com/study/algorithm/book/codingtest_learn/ch15/Main67.java
@@ -0,0 +1,12 @@
+package com.study.algorithm.book.codingtest_learn.ch15;
+
+public class Main67 {
+	public static void main(String[] args) {
+		int solution = solution("ABCBDAB", "BDCAB");
+		System.out.println("solution = " + solution);
+	}
+
+	private static int solution(String str1, String str2) {
+		return 0;
+	}
+}
---
+++ src/main/java/com/study/algorithm/book/codingtest_learn/ch15/Main68.java
@@ -0,0 +1,30 @@
+package com.study.algorithm.book.codingtest_learn.ch15;
+
+import java.util.Arrays;
+
+public class Main68 {
+
+	public static void main(String[] args) {
+		int solution1 = solution(new int[]{1, 4, 2, 3, 1, 5, 7, 3});
+		int solution2 = solution(new int[]{3, 2, 1});
+		System.out.println("solution = " + solution1);
+		System.out.println("solution = " + solution2);
+	}
+
+	private static int solution(int[] nums) {
+		int n = nums.length;
+
+		int[] dp = new int[n];
+		Arrays.fill(dp, 1);
+
+		for (int i = 1; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				if (nums[i] > nums[j]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+		}
+
+		return Arrays.stream(dp).max().getAsInt();
+	}
+}
