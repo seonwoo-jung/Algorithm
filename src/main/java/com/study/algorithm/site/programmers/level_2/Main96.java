96d168c1d5630af56834ab225b0931498c595a8d
seonwoo_jung <laborlawseon@gmail.com>
2024-12-02 20:28:31 +0900

    study algorithm

--- src/main/java/com/study/algorithm/site/programmers/level_2/Main96.java
+++ src/main/java/com/study/algorithm/site/programmers/level_2/Main96.java
@@ -1,24 +1,42 @@
 package com.study.algorithm.site.programmers.level_2;
 
+import java.util.Arrays;
+
+/**
+ * 입국심사
+ */
 public class Main96 {
 
-    public static void main(String[] args) {
-        int solution = solution(6, new int[]{7, 10});
-        System.out.println(solution);
-    }
+	public static void main(String[] args) {
+		long solution = solution(6, new int[]{7, 10});
+		System.out.println(solution);
+	}
 
-    private static int solution(int n, int[] times) {
-        int curTime = 0;
+	private static long solution(int n, int[] times) {
+		long answer = 0;
+		Arrays.sort(times);
 
-        while (n > 0) {
-            for (int i = 0; i < times.length; i++) {
-                if (curTime % times[i] == 0) {
-                    n--;
-                }
-            }
-            curTime++;
-        }
+		// 이분탐색을 위한 범위 설정
+		long lt = 0;
+		long rt = times[times.length - 1] * (long) n;
 
-        return curTime;
-    }
+		while (lt <= rt) {
+			long mid = (lt + rt) / 2;
+			long complete = 0;
+
+			for (int i = 0; i < times.length; i++) {
+				complete += mid / times[i];
+			}
+
+			// 해당 시간에는 모든 사람이 검사받을 수 없다.
+			if (complete < n) {
+				lt = mid + 1;
+			} else {
+				rt = mid - 1;
+				// 모두 검사받았으나, 더 최솟값이 있을 수 있다.
+				answer = mid;
+			}
+		}
+		return answer;
+	}
 }
