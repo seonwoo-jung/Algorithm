d95782b345e0a7b31bfaf997b81f06e26417beb9
seonwoo_jung <laborlawseon@gmail.com>
2024-12-09 20:34:54 +0900

    study algorithm

--- src/main/java/com/study/algorithm/site/backjoon/bj_24262.java
+++ src/main/java/com/study/algorithm/site/backjoon/bj_24262.java
@@ -7,7 +7,6 @@ public class bj_24262 {
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
 		long n = kb.nextLong();
-		System.out.println(n * n);
-		System.out.println(2);
+		System.out.println((n * (n - 1) * (n - 2) / 6) + "\n" + 3);
 	}
 }
