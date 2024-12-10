49fe2f30cd71da91b46ba9d6a54ccb1fb48b5786
seonwoo_jung <laborlawseon@gmail.com>
2024-12-10 21:14:39 +0900

    study algorithm

---
+++ src/main/java/com/study/algorithm/inflearn/middle/ch02/Main2.java
@@ -0,0 +1,31 @@
+package com.study.algorithm.inflearn.middle.ch02;
+
+import java.util.Arrays;
+
+/**
+ * 한 번 사용한 최초문자
+ */
+public class Main2 {
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(solution("aaabc")));
+		System.out.println(Arrays.toString(solution("aabb")));
+		System.out.println(Arrays.toString(solution("abcde")));
+		System.out.println(Arrays.toString(solution("abcdeabc")));
+		System.out.println(Arrays.toString(solution("abbccddee")));
+	}
+
+	private static int[] solution(String s) {
+		int[] answer = new int[5];
+		for (char x : s.toCharArray()) {
+			answer[x - 97]++;
+		}
+
+		int max = Arrays.stream(answer).max().getAsInt();
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = max - answer[i];
+		}
+
+		return answer;
+	}
+}
