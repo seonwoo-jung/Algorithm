231e3a0ead60e2c90b3c13fe85e527c5a335ac6b
seonwoo_jung <laborlawseon@gmail.com>
2024-12-13 18:00:24 +0900

    study algorithm

--- src/main/java/com/study/algorithm/inflearn/middle/ch03_datastructure/Main1.java
+++ src/main/java/com/study/algorithm/inflearn/middle/ch03_datastructure/Main1.java
@@ -1,5 +1,9 @@
 package com.study.algorithm.inflearn.middle.ch03_datastructure;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 // 최대 길이 연속 수열
 public class Main1 {
 
@@ -12,8 +16,41 @@ public class Main1 {
 	}
 
 	private static int solution(int[] nums) {
-		int answer = 0;
+		int answer = 1;
+		nums = Arrays.stream(nums).distinct().sorted().toArray();
+		System.out.println(Arrays.toString(nums));
+		for (int i = 0; i < nums.length - 1; i++) {
+			int val = nums[i];
+			int count = 1;
+			for (int j = i + 1; j < nums.length; j++) {
+				if (val + 1 == nums[j]) {
+					val = nums[j];
+					count++;
+				}
+			}
+			answer = Math.max(answer, count);
+		}
 
+		return answer;
+	}
+
+	private static int solution2(int[] nums) {
+		int answer = 0;
+		Set<Integer> set = new HashSet<>();
+		for (int x : nums) {
+			set.add(x);
+		}
+		for (int x : set) {
+			if (set.contains(x - 1)) {
+				continue;
+			}
+			int cnt = 0;
+			while (set.contains(x)) {
+				cnt++;
+				x++;
+			}
+			answer = Math.max(answer, cnt);
+		}
 		return answer;
 	}
 }
