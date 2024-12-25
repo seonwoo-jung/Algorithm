3b1ae4ac9cf5ada020855a6d3b333f07c5078709
seonwoo_jung <laborlawseon@gmail.com>
2024-12-25 15:22:38 +0900

    ## 학습내용 - DFS & BFS 학습

--- src/main/java/com/study/algorithm/inflearn/middle/ch07_bfs/Main5_집을짓자.java
+++ src/main/java/com/study/algorithm/inflearn/middle/ch07_bfs/Main5_집을짓자.java
@@ -1,5 +1,8 @@
 package com.study.algorithm.inflearn.middle.ch07_bfs;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main5_집을짓자 {
 
 	public static void main(String[] args) {
@@ -11,7 +14,43 @@ public class Main5_집을짓자 {
 
 	private static int solution(int[][] board) {
 		int answer = 0;
+		int[] dx = {-1, 0, 1, 0};
+		int[] dy = {0, 1, 0, -1};
+		int n = board.length;
+		int[][] dist = new int[n][n];
+		Queue<int[]> Q = new LinkedList<>();
+		int emptyLand = 0;
 
-		return answer;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (board[i][j] == 1) {
+					answer = Integer.MAX_VALUE;
+					Q.offer(new int[]{i, j});
+					int L = 0;
+
+					while (!Q.isEmpty()) {
+						L++;
+						int len = Q.size();
+						// 큐에 있는 노드들을 전부 꺼낸다.
+						for (int r = 0; r < len; r++) {
+							int[] cur = Q.poll();
+							for (int k = 0; k < 4; k++) {
+								int nx = cur[0] + dx[k];
+								int ny = cur[1] + dy[k];
+								if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == emptyLand) {
+									board[nx][ny]--;
+									dist[nx][ny] += L;
+									Q.offer(new int[]{nx, ny});
+									answer = Math.min(answer, dist[nx][ny]);
+								}
+							}
+						}
+					}
+
+					emptyLand--;
+				}
+			}
+		}
+		return answer == Integer.MAX_VALUE ? -1 : answer;
 	}
 }
--- src/main/java/com/study/algorithm/inflearn/middle/ch07_bfs/Main6_숲속의기사.java
+++ src/main/java/com/study/algorithm/inflearn/middle/ch07_bfs/Main6_숲속의기사.java
@@ -1,6 +1,12 @@
 package com.study.algorithm.inflearn.middle.ch07_bfs;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main6_숲속의기사 {
+
+	private static int[] dx = {-1, 0, 1, 0};
+	private static int[] dy = {0, 1, 0, -1};
 
 	public static void main(String[] args) {
 		System.out.println(solution(new int[][]{{4, 1, 0, 0, 0, 0, 1, 0}, {0, 0, 0, 1, 0, 1, 0, 0}, {0, 2, 1, 1, 3, 0, 4, 0}, {0, 0, 0, 4, 1, 1, 1, 0}}));
@@ -10,7 +16,47 @@ public class Main6_숲속의기사 {
 
 	private static int solution(int[][] board) {
 		int answer = 0;
+		int n = board.length;
+		Queue<Point> Q = new LinkedList<>();
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				if (board[i][j] == 2) {
+					Q.offer(new Point(i, j, false));
+				}
+			}
+		}
+		int L = 0;
+
+		while (!Q.isEmpty()) {
+			int len = Q.size();
+			for (int i = 0; i < len; i++) {
+				Point cur = Q.poll();
+				if (board[cur.x][cur.y] == 3 && cur.berry) {
+					return L;
+				}
+				for (int j = 0; j < 4; j++) {
+					int nx = cur.x + dx[j];
+					int ny = cur.y + dy[j];
+					if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] != 1) {
+						boolean isBerry = board[nx][ny] == 4;
+						Q.offer(new Point(nx, ny, isBerry));
+					}
+				}
+			}
+			L++;
+		}
 
 		return answer;
+	}
+
+	private static class Point {
+		public int x, y;
+		public boolean berry;
+
+		public Point(int x, int y, boolean berry) {
+			this.x = x;
+			this.y = y;
+			this.berry = berry;
+		}
 	}
 }
