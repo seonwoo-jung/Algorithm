92ea4296284fa9f92ca5b4feb1da9d5447566ba8
seonwoo_jung <laborlawseon@gmail.com>
2025-01-08 18:04:47 +0900

    ## 학습내용 - 그래프 문제 학습

--- src/main/java/com/study/algorithm/inflearn/middle/ch08_graph/Main2_최소환승경로.java
+++ src/main/java/com/study/algorithm/inflearn/middle/ch08_graph/Main2_최소환승경로.java
@@ -1,18 +1,56 @@
 package com.study.algorithm.inflearn.middle.ch08_graph;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main2_최소환승경로 {
 
-    public static void main(String[] args) {
-        System.out.println(solution(new int[][]{{1, 2, 3, 4, 5, 6, 19}, {2, 7, 8, 13}, {5, 9, 10}, {9, 11, 12, 18}, {13, 14, 15}, {14, 12, 16, 17}}, 1, 12));
-        System.out.println(solution(new int[][]{{1, 3, 5, 7}, {9, 3, 12}, {6, 5, 8}, {2, 8, 14, 15}, {2, 14, 16}}, 1, 14));
-        System.out.println(solution(new int[][]{{7, 12}, {5, 19}, {7, 19}, {9, 12, 13}, {9, 5, 15}}, 9, 19));
-        System.out.println(solution(new int[][]{{1, 2, 3, 4, 5}, {9, 7, 10}, {7, 6, 3, 8}, {5, 11, 8, 12}}, 1, 10));
-    }
+	public static void main(String[] args) {
+		System.out.println(solution(new int[][]{{1, 2, 3, 4, 5, 6, 19}, {2, 7, 8, 13}, {5, 9, 10}, {9, 11, 12, 18}, {13, 14, 15}, {14, 12, 16, 17}}, 1, 12));
+		System.out.println(solution(new int[][]{{1, 3, 5, 7}, {9, 3, 12}, {6, 5, 8}, {2, 8, 14, 15}, {2, 14, 16}}, 1, 14));
+		System.out.println(solution(new int[][]{{7, 12}, {5, 19}, {7, 19}, {9, 12, 13}, {9, 5, 15}}, 9, 19));
+		System.out.println(solution(new int[][]{{1, 2, 3, 4, 5}, {9, 7, 10}, {7, 6, 3, 8}, {5, 11, 8, 12}}, 1, 10));
+	}
 
-    private static int solution(int[][] routes, int s, int e) {
-        int answer = 0;
+	private static int solution(int[][] routes, int s, int e) {
+		int answer = 0;
+		HashMap<Integer, HashSet<Integer>> graph = new HashMap<>();
+		int n = routes.length;
 
-        return answer;
-    }
+		for (int i = 0; i < n; i++) {
+			for (int x : routes[i]) {
+				graph.putIfAbsent(x, new HashSet<>());
+				graph.get(x).add(i);
+			}
+		}
 
+		Queue<Integer> Q = new LinkedList<>();
+		int[] ch = new int[n];
+		Q.offer(s);
+		int L = 0;
+
+		while (!Q.isEmpty()) {
+			int len = Q.size();
+
+			for (int i = 0; i < len; i++) {
+				int curStop = Q.poll();
+				for (int line : graph.get(curStop)) {
+                    if (ch[line] == 1) {
+                        continue;
+                    }
+					ch[line] = 1;
+					for (int stop : routes[line]) {
+                        if (stop == e) {
+                            return L;
+                        }
+						Q.offer(stop);
+					}
+				}
+			}
+			L++;
+		}
+		return -1;
+	}
 }
--- src/main/java/com/study/algorithm/inflearn/middle/ch08_graph/Main3_벽허물기.java
+++ src/main/java/com/study/algorithm/inflearn/middle/ch08_graph/Main3_벽허물기.java
@@ -1,18 +1,61 @@
 package com.study.algorithm.inflearn.middle.ch08_graph;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 public class Main3_벽허물기 {
 
-    public static void main(String[] args){
-        System.out.println(solution(new int[][]{{0, 1, 1, 0}, {1, 0, 0, 1}, {0, 1, 0, 0}}));
+	private static int[] dx = {-1, 0, 1, 0};
+	private static int[] dy = {0, 1, 0, -1};
+	private static int[][] cost;
+
+	public static void main(String[] args) {
+		System.out.println(solution(new int[][]{{0, 1, 1, 0}, {1, 0, 0, 1}, {0, 1, 0, 0}}));
         System.out.println(solution(new int[][]{{0, 1, 1, 0},{1, 1, 0, 1},{0, 0, 1, 0}, {0, 1, 1, 1}, {0, 1, 1, 0}}));
         System.out.println(solution(new int[][]{{0, 1, 1, 0, 1, 1},{0, 1, 1, 1, 1, 1},{1, 0, 0, 0, 1, 1}, {1, 1, 0, 1, 1, 1}, {1, 1, 0, 1, 1, 0}, {1, 0, 0, 1, 1, 1}, {1, 1, 1, 1, 1, 0}}));
         System.out.println(solution(new int[][]{{0, 1, 1, 0, 1, 1, 1}, {1, 1, 1, 0, 1, 1, 1}, {1, 0, 0, 0, 0, 1, 1}, {1, 1, 1, 0, 1, 1, 1}, {1, 1, 1, 0, 1, 1, 0}, {1, 0, 1, 0, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 0}}));
         System.out.println(solution(new int[][]{{0, 0, 1, 0, 1, 1, 1},{1, 1, 0, 0, 1, 1, 1},{1, 1, 0, 1, 0, 1, 1}, {0, 0, 1, 0, 1, 1, 1}, {1, 0, 1, 0, 1, 1, 0}, {1, 0, 1, 0, 1, 1, 1}, {1, 0, 0, 1, 1, 1, 1}, {1, 1, 0, 0, 1, 1, 1}, {1, 1, 0, 1, 1, 1, 0}}));
-    }
+	}
 
-    private static int solution(int[][] board) {
-        int answer = 0;
+	private static int solution(int[][] board) {
+		int n = board.length;
+		int m = board[0].length;
+		cost = new int[n][m];
 
-        return answer;
-    }
+		for (int i = 0; i < n; i++) {
+			Arrays.fill(cost[i], Integer.MAX_VALUE);
+		}
+
+		cost[0][0] = 0;
+		// 우선순위 큐로 인해서 벽을 적게 부순 경로를 먼저 탐색한다.
+		PriorityQueue<int[]> PQ = new PriorityQueue<>((a, b) -> Integer.compare(a[2], b[2]));
+
+		// x, y, 벽을 부순 개수
+		PQ.add(new int[]{0, 0, 0});
+
+		while (!PQ.isEmpty()) {
+			int[] cur = PQ.poll();
+
+			// 탐색하려는 대상이 벽을 부순 개수가 많으면 탐색할 필요 X
+			if (cur[2] > cost[cur[0]][cur[1]]) continue;
+
+			// 레벨탐색 시작
+			for (int k = 0; k < 4; k++) {
+				int nx = cur[0] + dx[k];
+				int ny = cur[1] + dy[k];
+
+				// 격자 밖 continue
+				if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+
+				if (board[nx][ny] == 0 && cost[nx][ny] > cur[2]) {
+					cost[nx][ny] = cur[2];
+					PQ.offer(new int[]{nx, ny, cur[2]});
+				} else if (board[nx][ny] == 1 && cost[nx][ny] > cur[2] + 1) {
+					cost[nx][ny] = cur[2] + 1;
+					PQ.offer(new int[]{nx, ny, cur[2] + 1});
+				}
+			}
+		}
+		return cost[n - 1][m - 1];
+	}
 }
