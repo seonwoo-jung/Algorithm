e35f142c34e07e4a7fa6019ac0db2f418fc465bb
seonwoo_jung <laborlawseon@gmail.com>
2025-01-09 15:45:02 +0900

    ## 학습내용 - 그래프 문제 학습

--- src/main/java/com/study/algorithm/inflearn/middle/ch08_graph/Main4_방향바꾸기.java
+++ src/main/java/com/study/algorithm/inflearn/middle/ch08_graph/Main4_방향바꾸기.java
@@ -1,17 +1,61 @@
 package com.study.algorithm.inflearn.middle.ch08_graph;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Main4_방향바꾸기 {
-    public static void main(String[] args){
-        System.out.println(solution(new int[][]{{3, 1, 3}, {1, 4, 2}, {4, 2, 3}}));
-        System.out.println(solution(new int[][]{{3, 2, 1, 3}, {1, 1, 4, 2}, {3, 4, 2, 1}, {1, 2, 2, 4}}));
-        System.out.println(solution(new int[][]{{3, 2, 1, 3, 1, 2}, {2, 1, 1, 1, 4, 2}, {2, 2, 2, 1, 2, 2}, {1, 3, 3, 4, 4, 4}, {1, 2, 2, 3, 3, 4}}));
-        System.out.println(solution(new int[][]{{3, 2, 1, 3, 1, 2, 2, 2}, {2, 1, 1, 1, 4, 2, 1, 1}, {2, 2, 2, 1, 2, 2, 3, 4}, {1, 3, 3, 4, 4, 4, 3, 1}, {1, 2, 2, 3, 3, 4, 3, 4}, {1, 2, 2, 3, 3, 1, 1, 1}}));
-        System.out.println(solution(new int[][]{{1, 2, 3, 2, 1, 3, 1, 2, 2, 2}, {1, 2, 2, 1, 1, 1, 4, 2, 1, 1}, {3, 2, 2, 2, 2, 1, 2, 2, 3, 4}, {3, 3, 1, 3, 3, 4, 4, 4, 3, 1}, {1, 1, 1, 2, 2, 3, 3, 4, 3, 4}, {1, 1, 1, 2, 2, 3, 3, 1, 1, 1}}));
-    }
 
-    private static int solution(int[][] board) {
-        int answer = 0;
+	private static int[] dx = {0, 0, 1, -1};
+	private static int[] dy = {1, -1, 0, 0};
+	private static int[][] cost;
 
-        return answer;
-    }
+	public static void main(String[] args) {
+		System.out.println(solution(new int[][]{{3, 1, 3}, {1, 4, 2}, {4, 2, 3}}));
+		System.out.println(solution(new int[][]{{3, 2, 1, 3}, {1, 1, 4, 2}, {3, 4, 2, 1}, {1, 2, 2, 4}}));
+		System.out.println(solution(new int[][]{{3, 2, 1, 3, 1, 2}, {2, 1, 1, 1, 4, 2}, {2, 2, 2, 1, 2, 2}, {1, 3, 3, 4, 4, 4}, {1, 2, 2, 3, 3, 4}}));
+		System.out.println(solution(new int[][]{{3, 2, 1, 3, 1, 2, 2, 2}, {2, 1, 1, 1, 4, 2, 1, 1}, {2, 2, 2, 1, 2, 2, 3, 4}, {1, 3, 3, 4, 4, 4, 3, 1}, {1, 2, 2, 3, 3, 4, 3, 4}, {1, 2, 2, 3, 3, 1, 1, 1}}));
+		System.out.println(solution(new int[][]{{1, 2, 3, 2, 1, 3, 1, 2, 2, 2}, {1, 2, 2, 1, 1, 1, 4, 2, 1, 1}, {3, 2, 2, 2, 2, 1, 2, 2, 3, 4}, {3, 3, 1, 3, 3, 4, 4, 4, 3, 1}, {1, 1, 1, 2, 2, 3, 3, 4, 3, 4}, {1, 1, 1, 2, 2, 3, 3, 1, 1, 1}}));
+	}
+
+	private static int solution(int[][] board) {
+		int answer = 0;
+		int n = board.length;
+		int m = board[0].length;
+		cost = new int[n][m];
+
+		for (int i = 0; i < cost.length; i++) {
+			Arrays.fill(cost[i], Integer.MAX_VALUE);
+		}
+		cost[0][0] = 0;
+
+		Queue<int[]> PQ = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[2], o2[2]));
+		// x, y, 화살표 바꾼 횟수
+		PQ.offer(new int[]{0, 0, 0});
+
+		while (!PQ.isEmpty()) {
+			int[] cur = PQ.poll();
+			int dir = board[cur[0]][cur[1]] - 1;
+
+			// 더 큰 값으로 탐색할 필요 X
+			if (cur[2] > cost[cur[0]][cur[1]]) continue;
+
+			for (int k = 0; k < 4; k++) {
+				int nx = cur[0] + dx[k];
+				int ny = cur[1] + dy[k];
+				if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+				// 화살표를 바꿀 필요가 없음
+				if (k == dir && cost[nx][ny] > cur[2]) {
+					cost[nx][ny] = cur[2];
+					PQ.offer(new int[]{nx, ny, cur[2]});
+				} else {
+					if (cost[nx][ny] > cur[2] + 1) {
+						cost[nx][ny] = cur[2] + 1;
+						PQ.offer(new int[]{nx, ny, cur[2] + 1});
+					}
+				}
+			}
+		}
+		return cost[n - 1][m - 1];
+	}
 }
--- src/main/java/com/study/algorithm/inflearn/middle/ch08_graph/Main5_공굴리기.java
+++ src/main/java/com/study/algorithm/inflearn/middle/ch08_graph/Main5_공굴리기.java
@@ -1,16 +1,57 @@
 package com.study.algorithm.inflearn.middle.ch08_graph;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 public class Main5_공굴리기 {
-    public static void main(String[] args) {
+
+	public static void main(String[] args) {
         System.out.println(solution(new int[][]{{0, 0, 1, 0, 0, 0}, {0, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 1, 0}, {1, 0, 1, 1, 1, 0}, {1, 0, 0, 0, 0, 0}}, new int[]{1, 0}, new int[]{4, 5}));
-        System.out.println(solution(new int[][]{{0, 0, 1, 0, 0, 0}, {0, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 1, 0}, {1, 0, 1, 1, 1, 0}, {1, 0, 0, 0, 0, 0}}, new int[]{0, 0}, new int[]{4, 2}));
+		System.out.println(solution(new int[][]{{0, 0, 1, 0, 0, 0}, {0, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 1, 0}, {1, 0, 1, 1, 1, 0}, {1, 0, 0, 0, 0, 0}}, new int[]{0, 0}, new int[]{4, 2}));
         System.out.println(solution(new int[][]{{1, 0, 1, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 1, 0}, {1, 1, 0, 1, 1}, {0, 0, 0, 0, 0}}, new int[]{0, 3}, new int[]{4, 2}));
         System.out.println(solution(new int[][]{{0, 1, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 0}, {0, 1, 1, 0, 1, 1}, {0, 0, 0, 0, 0, 0}}, new int[]{0, 0}, new int[]{4, 5}));
         System.out.println(solution(new int[][]{{0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 0, 0, 0}, {0, 0, 1, 0, 0, 0, 0, 0}}, new int[]{0, 0}, new int[]{4, 3}));
-    }
+	}
 
-    private static int solution(int[][] board, int[] s, int[] e) {
-        int answer = 0;
-        return answer;
-    }
+	private static int solution(int[][] board, int[] s, int[] e) {
+		int n = board.length;
+		int m = board[0].length;
+		int[][] cost = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			Arrays.fill(cost[i], Integer.MAX_VALUE);
+		}
+
+		PriorityQueue<int[]> PQ = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+		PQ.add(new int[]{s[0], s[1], 0});
+		cost[s[0]][s[1]] = 0;
+
+		while (!PQ.isEmpty()) {
+			int[] cur = PQ.poll();
+
+			if (cur[2] > cost[cur[0]][cur[1]]) continue;
+
+			for (int[] dir : new int[][]{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}) {
+				int nx = cur[0];
+				int ny = cur[1];
+				int len = cur[2];
+				while (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 0) {
+					nx += dir[0];
+					ny += dir[1];
+					len++;
+				}
+				nx -= dir[0];
+				ny -= dir[1];
+				len--;
+				if (cost[nx][ny] > len) {
+					cost[nx][ny] = len;
+					PQ.add(new int[]{nx, ny, len});
+				}
+			}
+		}
+		if (cost[e[0]][e[1]] == Integer.MAX_VALUE) {
+			return -1;
+		} else {
+			return cost[e[0]][e[1]];
+		}
+	}
 }
