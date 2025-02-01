5ac73912799e392012009f6f587db76bd39c671e
seonwoo_jung <laborlawseon@gmail.com>
2025-02-01 14:53:28 +0900

    ## 학습내용 - QuickSort 학습

--- src/main/java/com/study/algorithm/personal/study/other/QuickSort.java
+++ src/main/java/com/study/algorithm/personal/study/other/QuickSort.java
@@ -1,49 +1,48 @@
 package com.study.algorithm.personal.study.other;
 
+import java.util.Arrays;
+
+/**
+ * pivot을 무작위로 정하는데, 계속 가장 작은 값만 정해지면 O(n^2)의 시간복잡도를 가진다 최악의 경우 O(n^2)까지 걸릴 수 있고, Unstable Sort임
+ */
 public class QuickSort {
+
 	public static void main(String[] args) {
 		int[] arr = {5, 3, 7, 6, 2, 9, 1};
-
 		quickSort(arr, 0, arr.length - 1);
+		System.out.println(Arrays.toString(arr));
 	}
 
-	// QuickSort function
-	public static void quickSort(int[] arr, int low, int high) {
-		if (low < high) {
-			// Partition the array and get the pivot index
-			int pivotIndex = partition(arr, low, high);
+	private static void quickSort(int[] arr, int start, int end) {
+		int part2 = partition(arr, start, end);
 
-			// Recursively sort elements before and after the partition
-			quickSort(arr, low, pivotIndex - 1);
-			quickSort(arr, pivotIndex + 1, high);
+		if (start < part2 - 1) {
+			quickSort(arr, start, part2 - 1);
+		}
+
+		if (part2 < end) {
+			quickSort(arr, part2, end);
 		}
 	}
 
-	// Partition function
-	public static int partition(int[] arr, int low, int high) {
-		int pivot = arr[high]; // Choose the last element as the pivot
-		int i = low - 1;       // Index for the smaller element
+	private static int partition(int[] arr, int start, int end) {
+		int pivot = arr[(start + end) / 2];
 
-		for (int j = low; j < high; j++) {
-			// If the current element is smaller than or equal to the pivot
-			if (arr[j] <= pivot) {
-				i++; // Increment the index of the smaller element
-
-				// Swap arr[i] and arr[j]
-				swap(arr, i, j);
+		while (start <= end) {
+			while (arr[start] < pivot) start++;
+			while (arr[end] > pivot) end--;
+			if (start <= end) {
+				swap(arr, start, end);
+				start++;
+				end--;
 			}
 		}
-
-		// Swap arr[i + 1] and the pivot (arr[high])
-		swap(arr, i + 1, high);
-
-		return i + 1; // Return the pivot index
+		return start;
 	}
 
-	// Swap function to exchange two elements
-	public static void swap(int[] arr, int i, int j) {
-		int temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
+	private static void swap(int[] arr, int start, int end) {
+		int tmp = arr[start];
+		arr[start] = arr[end];
+		arr[end] = tmp;
 	}
 }
